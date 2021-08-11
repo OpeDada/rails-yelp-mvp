fd49a9856b81f128bc599aa3569b62199c470f2d
OpeDada <opedada@gmail.com>
2021-08-11 20:14:02 +0900

    yelp MVP still working on it

--- Gemfile
+++ Gemfile
@@ -42,7 +42,11 @@ end
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
-# echo "gem 'rspec-rails', group: [ :test ]"
-# echo "gem 'rails-controller-testing', group: [ :test ]"
 gem 'rspec-rails', group: [ :test ]
 gem 'rails-controller-testing', group: [ :test ]
+
+gem 'autoprefixer-rails'
+gem 'font-awesome-sass', '~> 5.6.1'
+gem 'simple_form'
+
+gem 'faker'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+
+5.times do
+  @restaurant = Restaurant.create!(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    category: Restaurant::CATEGORIES.sample,
+    phone_number: Faker::PhoneNumber.cell_phone,
+    # rating: rand(0..5)
+  )
+end
+puts "Created #{Restaurant.count} restaurants."
